d95c489f98209bea3624eea352e99c42d0dabf20
kaung myat <kaungmthuqa@gmail.com>
2026-01-21 18:36:04 -0800

    add all files

---
+++ week-2/week-2-Assignment.js
@@ -0,0 +1,17 @@
+//1 .Create two numbers and:
+//1. Create two numbers and:
+//  - Add them
+//- Subtract them
+// - Multiply them
+// - Divide them
+let myNum1 = 10;
+let myNum2 = 5;
+
+let add = myNum1 + myNum2;
+let subtract = myNum1 - myNum2;
+let multiply = myNum1 * myNum2;
+let divide = myNum1 / myNum2;
+console.log(add);
+console.log(subtract);
+console.log(multiply);
+console.log(divide);
